825b0e03bd14e84f15bf9c0cbaad9cd81e59a9ae
Cheol-Cheol <dream174822@gmail.com>
2022-03-16 01:03:39 +0900

    📝 입출력과 사칙연산

---
+++ BOJ/입출력과사칙연산/b18108/app.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+input = input.map((item) => +item);
+solution(input[0]);
+
+function solution(A) {
+  console.log(A - 543);
+}
