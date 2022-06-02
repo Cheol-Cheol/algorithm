2a3dde7f052039136b7ea2bce04b8fc95edc00f7
Cheol-Cheol <dream174822@gmail.com>
2022-06-02 23:57:53 +0900

    📝 수정

--- BOJ/기본수학1/b10757/app.js
+++ BOJ/기본수학1/b10757/app.js
@@ -4,4 +4,4 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 input = input[0].split(" ");
 const A = BigInt(input[0]);
 const B = BigInt(input[1]);
-console.log((A + B).toString());
+console.log(A + B);
