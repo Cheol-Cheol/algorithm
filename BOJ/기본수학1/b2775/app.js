1e67982940002b56ab78a44b755e671e4c5c3db2
Cheol-Cheol <dream174822@gmail.com>
2022-06-12 19:58:25 +0900

    📝 수정

--- BOJ/기본수학1/b2775/app.js
+++ BOJ/기본수학1/b2775/app.js
@@ -7,7 +7,7 @@ const TESTCASE = input.shift();
 for (let i = 0; i < TESTCASE; i++) {
   const k = +input.shift();
   const n = +input.shift();
-  // k:층 n:호
+
   const house = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
   // 0층 (0층의 i호에는 i명이 산다.)
   for (let i = 1; i <= n; i++) {
--- BOJ/기본수학2/b1929/app.js
+++ BOJ/기본수학2/b1929/app.js
@@ -2,4 +2,32 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [M, N] = input[0].split(" ").map((item) => +item);
+const [n, m] = input[0].split(" ").map((item) => +item);
+
+// 에라토스테네스의 체
+const prime = { 1: true };
+
+for (let i = 2; i <= Math.sqrt(m); i++) {
+  if (prime[i]) continue;
+  for (let j = i ** 2; j <= m; j += i) {
+    prime[j] = true;
+  }
+}
+const results = [];
+
+for (let i = n; i <= m; i++) {
+  if (!prime[i]) results.push(i);
+}
+console.log(results.join("\n"));
+
+// function isPrime(num) {
+//   if (num === 1) return;
+//   for (let j = 2; j <= Math.sqrt(num); j++) {
+//     if (num % j === 0) return;
+//   }
+//   return console.log(num);
+// }
+
+// for (let i = M; i <= N; i++) {
+//   isPrime(i);
+// }
